23cddf9170b1c3bb6d8fe1cc32042961c0ee78ea
palomaedeltrudes <lomita.trudys@gmail.com>
2023-08-09 21:46:39 -0300

    update exercicio-4.py

--- python_zero_ao_ds/exercicio-4.py
+++ python_zero_ao_ds/exercicio-4.py
@@ -2,11 +2,10 @@ numero1 = input()
 numero2 = input()
 numero3 = input()
 
-if numero1 > numero2 and numero1 > numero3:
-    print("O maior valor é: ", numero1)
+if numero1 > numero2:
+    if numero1 > numero3:
+        print("O maior valor é: ", numero1)
 else:
-    print("O maior valor é: ", numero3)
-
     if numero2 > numero3:        
         print("O maior valor é: ", numero2)
     else:
