d64932d84ef57064bf96b9e5e8a0b001f090ca68
palomaedeltrudes <lomita.trudys@gmail.com>
2023-08-14 23:18:01 -0300

    Update exerc06.py

--- python_zero_ao_ds/exerc06.py
+++ python_zero_ao_ds/exerc06.py
@@ -1,20 +1,13 @@
-numero1 = input("Digite o numero 1: ")
-numero2 = input("Digite o numero 2: ")
-numero3 = input("Digite o numero 3: ")
+numero1 = float(input("Digite o numero 1: "))
+numero2 = float(input("Digite o numero 2: "))
+numero3 = float(input("Digite o numero 3: "))
 
-if numero1 > numero2:
-    if numero1 > numero3:
-        print("O maior valor é: ", numero1)
-else:
-    if numero2 > numero3:        
-        print("O maior valor é: ", numero2)
-    else:
-        print("O maior valor é: ", numero3)
-if numero1 < numero2:
-    print("O menor valor é: ", numero1)
-else:
-    if numero2 < numero3:        
-        print("O menor valor é: ", numero2)
-    else:
-        print("O menor valor é: ", numero3)
+maior = max(numero1, numero2, numero3)   
+
+menor = min(numero1, numero2, numero3)
+
+print(f"O maior numero é {maior}")
+
+print(f"O menor numero é {menor}")  
+
         
